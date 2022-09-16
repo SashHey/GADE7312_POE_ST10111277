8ac9ae2af3b32ad2c022093e156120af1ad1b8a5
DanteDercksen <Ruan@draco.co.za>
2022-09-16 20:16:28 +0200

    Final Commit Part 1

---
+++ HeightMap.cpp
@@ -0,0 +1,60 @@
+#include "HeightMap.h"
+#include "Texture.h"
+#include <iostream>
+#include <GL/glut.h>
+#include <random>
+#include "Texture.h"
+
+void HeightMap::DrawMap(float maxHeight, float size, Texture* texture) {
+
+	glPushMatrix();
+	glTranslatef(-size / 2.0, 0, -size/ 2.0);
+
+	for (int y = 0; y < height - 1; y++)
+	{
+		for (int x = 0; x  < width - 1; x ++)
+		{
+
+			Pixel p0 = texture->getPixelAt(1, 1);
+			Pixel p1 = texture->getPixelAt(x,y);
+			Pixel p2 = texture->getPixelAt(x, y + 1);
+			Pixel p3 = texture->getPixelAt(x + 1, y);
+			Pixel p4 = texture->getPixelAt(x + 1, y + 1);
+
+			float vy1 = p1.r * maxHeight;
+			float vx1 = (float)x / (float)width * size;
+			float vz1 = (float)y/ (float)height * size;
+
+			float vy2 = p2.r * maxHeight;
+			float vx2 = (float)x / (float)width * size;
+			float vz2 = (float)(y + 1) / (float)height * size;
+
+			float vy3 = p3.r * maxHeight;
+			float vx3 = (float)(x + 1) / (float)width * size;
+			float vz3 = (float)y/ (float)height * size;
+
+			float vy4 = p4.r * maxHeight;
+			float vx4 = (float)(x + 1) / (float)width * size;
+			float vz4 = (float)(y + 1) / (float)height * size;
+			
+
+			glBegin(GL_TRIANGLE_STRIP);
+			{
+				glColor3f(p1.r, p1.r, p1.r);
+				glVertex3f(vx1, vy1, vz1);
+
+				glColor3f(p2.r, p2.r, p2.r);
+				glVertex3f(vx2, vy2, vz2);
+
+				glColor3f(p3.r, p3.r, p3.r);
+				glVertex3f(vx3, vy3, vz3);
+
+				glColor3f(p4.r, p4.r, p4.r);
+				glVertex3f(vx4, vy4, vz4);
+			}
+			glEnd();
+		}
+	}
+
+	glPopMatrix();
+}
---
+++ Main.cpp
@@ -0,0 +1,212 @@
+#include <GL/glut.h>
+#include <iostream>
+#include <math.h>
+#include "GameObject.h"
+#include "Pixel.h"
+#include "Texture.h"
+#include "TexturedCube.h"
+#include "ChessBoard.h"
+#include "HeightMap.h"
+#include <Windows.h>
+
+
+//STB
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#define LEFT_ARROW 75
+#define RIGHT_ARROW 77
+
+using namespace std;
+
+
+static const int WIDTH = 800;
+static const int HEIGHT = 600;
+
+GameObject gameObject;
+Texture* texture;
+TexturedCube gObject;
+ChessBoard chessBoard;
+HeightMap heightMap;
+
+void input(int key, int x, int y);
+void init();
+void display();
+void timer(int);
+void updateCamera();
+int cameraPos;
+
+bool isPressed = false;
+
+//Initial Calls + Window Setup
+int main(int argc, char* argv[]) {
+
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GL_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	
+	//Window initialising
+	{
+		int windowX = (int)(glutGet(GLUT_SCREEN_WIDTH) - WIDTH) / 2;
+		int windowY = (int)(glutGet(GLUT_SCREEN_HEIGHT) - HEIGHT) / 2;
+		glutInitWindowPosition(windowX, windowY);
+		glutInitWindowSize(WIDTH, HEIGHT);
+		glutCreateWindow("My First Window");
+	}
+
+	chessBoard.SetRandom(); //Sets the offset for each chess tile
+
+	glutDisplayFunc(display);
+	glutSpecialFunc(input);
+	glutTimerFunc(0, timer, 0);
+	init();
+	glutMainLoop();
+	
+	delete texture;
+
+	return 0;
+}
+
+//Change Initial Camera View + Texture
+void init() {
+	glEnable(GL_DEPTH_TEST);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	//glOrtho(0, WIDTH, HEIGHT, 0,0,1);
+	gluPerspective(50.0, (double)WIDTH / (double)HEIGHT, 1.0, 1000.0);
+
+	//updateCamera();
+	gluLookAt(
+		0, 30, 50,
+		0, 0, 0,
+		0, 1, 0
+	);
+
+	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+	texture = new Texture("Textures/cloudimage.png");
+	//texture = new Texture("Textures/wolfram.png");
+}
+
+void display() {
+
+	updateCamera();
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glRotatef(1, 0, 1, 0);
+
+	//Display HeightMap
+	{
+	glPushMatrix();
+	heightMap.DrawMap(15, 100, texture);
+	glPopMatrix();
+	}
+
+	//Display ChessBoard
+	{
+	glPushMatrix();
+	glTranslatef(0, 0.5f, 0);
+	glRotatef(90, 1, 0, 0);
+	chessBoard.draw();
+	glPopMatrix();
+	}
+
+	glutSwapBuffers();
+
+}
+
+void timer(int) {
+	glutPostRedisplay();
+	glutTimerFunc(1000/60, timer, 0);		//	Comment this if you want to pause
+}
+
+void updateCamera() {
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	double eyeX = 0;
+
+
+	if (!isPressed)
+	{
+		if (GetKeyState(GLUT_KEY_RIGHT) & 0x8000)
+		{
+			cout << "D" << endl;
+			isPressed = true;
+			cameraPos--;
+		}
+		else if (GetKeyState(GLUT_KEY_LEFT) & 0x8000)
+		{
+			
+			cout << "A" << endl;
+			isPressed = true;
+			cameraPos++;
+		}
+	}
+	else
+	{
+		Sleep(500);
+		isPressed = false;
+	}
+
+	//Check cameraPos
+	{
+		if (cameraPos > 2) cameraPos = 0;
+		if (cameraPos < 0) cameraPos = 2;
+		if (cameraPos == 0)
+		{
+			eyeX = -5; 
+		}
+		if (cameraPos == 2)
+		{
+			eyeX = 5;
+		}
+	}
+
+	gluLookAt(
+		0, 0, 5,
+		eyeX,0,0,
+		0, 1, 0
+	);
+}
+
+void input(int key, int x, int y) {
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	double eyeX = 0;
+
+
+	if (key == (GLUT_KEY_RIGHT))
+	{
+		cout << "D" << endl;
+		isPressed = true;
+		cameraPos--;
+	}
+	else if (key == (GLUT_KEY_LEFT))
+	{
+
+		cout << "A" << endl;
+		isPressed = true;
+		cameraPos++;
+	}
+
+	//Check cameraPos
+	{
+		if (cameraPos > 2) cameraPos = 0;
+		if (cameraPos < 0) cameraPos = 2;
+		if (cameraPos == 0)
+		{
+			eyeX = -5;
+		}
+		if (cameraPos == 2)
+		{
+			eyeX = 5;
+		}
+	}
+
+	gluLookAt(
+		0, 0, 5,
+		eyeX, 0, 0,
+		0, 1, 0
+	);
+}
+
